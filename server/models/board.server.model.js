76499241046fe6f80191a1bb6ca95ea2cfbbca58
william huang <will.h86@gmail.com>
2015-01-28 22:56:04 -0800

    schema update

--- server/models/board.server.model.js
+++ server/models/board.server.model.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var BoardSchema = new Schema({
+  floor: String,
+  order: Number,
   data: Schema.Types.Mixed,
   updated: {type: Date, default: Date.now},
   isActive: Boolean,
