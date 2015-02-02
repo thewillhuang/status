496408590b2a7b9f1fe77775450cd37043f2af40
Victor Leung <victorleungtw@gmail.com>
2015-02-02 21:12:42 +0800

    fix board schema

--- server/models/board.server.model.js
+++ server/models/board.server.model.js
@@ -4,13 +4,30 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var BoardSchema = new Schema({
-  floor: String,
-  order: Number,
+  floor: {
+    type: String,
+    default: ''
+  },
+  order: {
+   type: Number
+  },
   data: Schema.Types.Mixed,
-  updated: {type: Date, default: Date.now},
-  isActive: Boolean,
-  history: String,
-  updateBy: String
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  isActive: {
+    type: Boolean,
+    default: ''
+  },
+  history: {
+    type: String,
+    default: ''
+  },
+  updateBy: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
 });
 
 mongoose.model('Board', BoardSchema);
